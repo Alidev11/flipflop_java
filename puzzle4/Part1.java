f4fb7f154f8b68815ed0a0da713336b8bc3cb2bf
Alidev11 <karwawiali@gmail.com>
2026-01-30 11:55:04 +0100

    Succeeded Puzzle4 part1

--- puzzle4/Part1.java
+++ puzzle4/Part1.java
@@ -6,16 +6,20 @@ import java.util.*;
 public class Part1{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int ans = 0, max = Integer.MIN_VALUE;
+        int ans = 0;
+        int[] cache = new int[]{0,0};
 
         while(sc.hasNext()){
             String line = sc.nextLine();
+            String[] coordinates = line.split(",");
+            int[] coordinatesInt = Arrays.stream(coordinates).mapToInt(Integer::parseInt).toArray();
             
-            
+            ans += Math.abs(coordinatesInt[0] - cache[0]) + Math.abs(coordinatesInt[1] - cache[1]);
+            cache[0] = coordinatesInt[0];
+            cache[1] = coordinatesInt[1];
         }
 
-        System.out.println(max);
-
+        System.out.println(ans);
         sc.close();
     }
 }
